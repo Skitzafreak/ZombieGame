a39174f8d4135e57283e88772c3af125c97abb61
Dajh <Jessemcalister1988@gmail.com>
2015-12-11 04:02:03 -0500

    Update Level.h

--- ZombieGame/Level.h
+++ ZombieGame/Level.h
@@ -23,13 +23,16 @@ public:
     const std::vector<std::string>& getLevelData() const { return _levelData; }
     glm::vec2 getStartPlayerPos() const { return _startPlayerPos; }
     const std::vector<glm::vec2>& getZombieStartPositions() const { return _zombieStartPositions; }
-
+	const std::vector<glm::vec2>& getAmmoStartPositions() const { return _ammoStartPositions; }
+	const std::vector<glm::vec2>& getPowerUPStartPosition() const { return _powerUPstartPositions; }
 private:
     std::vector<std::string> _levelData;
     int _numHumans;
     Bengine::SpriteBatch _spriteBatch;
 
     glm::vec2 _startPlayerPos;
+	std::vector<glm::vec2> _ammoStartPositions;
     std::vector<glm::vec2> _zombieStartPositions;
+	std::vector<glm::vec2> _powerUPstartPositions;
 };
 
