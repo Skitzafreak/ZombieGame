6094716b4d060268cc4babb80b8885d3aa93ecc1
Dajh <Jessemcalister1988@gmail.com>
2015-12-11 04:08:24 -0500

    Update Gun.h

--- ZombieGame/Gun.h
+++ ZombieGame/Gun.h
@@ -8,32 +8,35 @@
 class Gun
 {
 public:
-    Gun(std::string name, int fireRate, int bulletsPerShot, float spread, float bulletDamage, float bulletSpeed);
-    ~Gun();
+	Gun(std::string name, int fireRate, int bulletsPerShot, float spread, float bulletDamage, float bulletSpeed, int ammo, int upgrades = 0);
+	~Gun();
 
-    void update(bool isMouseDown, const glm::vec2& position, const glm::vec2& direction, std::vector<Bullet>& bullets);
+	void update(bool isMouseDown, const glm::vec2& position, const glm::vec2& direction, std::vector<Bullet>& bullets);
+	void upgrade();
 	void addAmmo(int ammo);
 	int getAmmo();
 	std::string getName();
+
 private:
 
-    void fire(const glm::vec2& direction, const glm::vec2& position, std::vector<Bullet>& bullets);
+	void fire(const glm::vec2& direction, const glm::vec2& position, std::vector<Bullet>& bullets);
 
-    std::string _name;
+	std::string _name;
 
-    int _fireRate; ///< Fire rate in terms of frames
+	int _fireRate; ///< Fire rate in terms of frames
 
-    int _bulletsPerShot; ///< How many bullets are fired at at time
+	int _bulletsPerShot; ///< How many bullets are fired at at time
 
-    float _spread; ///< Accuracy
+	float _spread; ///< Accuracy
 
-    float _bulletSpeed;
+	float _bulletSpeed;
 
-    int _bulletDamage;
+	int _bulletDamage;
 
-    int _ammo;
+	int _ammo;
 
-    int _frameCounter; ///< Counts frames so we know when to shoot bullets
+	int _upgrades;
+
+	int _frameCounter; ///< Counts frames so we know when to shoot bullets
 
 };
-
